7cd2f1d8ac6c64b4f7d821bf6c84e5ce0bd17311
goozeberry <161857405+goozeberry@users.noreply.github.com>
2024-03-01 15:26:28 +0000

    fix modal honk

--- src/frontend/src/components/NoticeModalContent.tsx
+++ src/frontend/src/components/NoticeModalContent.tsx
@@ -2,9 +2,7 @@ export const NoticeModalContent: React.FC = () => {
   return (
     <div className="flex w-full flex-col items-center gap-6 py-8">
       <p className="font-jost text-3xl font-bold text-white">Notice</p>
-      <p className="">
-        This is a mainnet interface pal
-      </p>
+      <p className="">This is a mainnet interface pal</p>
     </div>
   );
 };
